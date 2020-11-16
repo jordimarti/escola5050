d4bab973fd444126b9543c24dfa3500a50eb03bd
Jordi Martí Muñoz <jordimartimunoz@iMac-de-Jordi.local>
2020-11-16 17:33:57 +0100

    Afegeix opció casa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index, :casa]
   
   def index
   	@missatge = Missatge.new
@@ -9,5 +9,11 @@ class HomeController < ApplicationController
 
   def video
   	@video = 'wHiat6z6W00dCJjlnoLnx5Fln2mzR9bnuWRIlHeSjZ01E'
+  end
+
+  def casa
+  	@missatge = Missatge.new
+    @missatges = Missatge.all
+    @video = 'wHiat6z6W00dCJjlnoLnx5Fln2mzR9bnuWRIlHeSjZ01E'
   end
 end
